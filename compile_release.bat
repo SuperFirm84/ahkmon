63ce07b2e6a8053861b8b4722c9352bd755b78f4
Joey <17505625+jmctune@users.noreply.github.com>
2021-05-08 20:49:07 -0500

    removing dq10dialog and updating compile

--- compile_release.bat
+++ compile_release.bat
@@ -1,4 +1,4 @@
 @echo off
-set /p release="New release version (last octet) (ex: 3 for v1.1.3): "
+set /p release="New release version (last octet) (ex: 3 for v1.2.3): "
 "C:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe" /in "ahkmon.ahk" /icon "imgs/dqx.ico"
-"C:\Program Files\7-Zip\7z.exe" a -tzip ahkmon_v1.1.%release%.zip ./apps ./imgs ./lib ahkmon.ahk ahkmon.exe dqxtrl.db sqlite3.dll
+"C:\Program Files\7-Zip\7z.exe" a -tzip ahkmon_v1.2.%release%.zip ./imgs ahkmon.exe dqxtrl.db sqlite3.dll
